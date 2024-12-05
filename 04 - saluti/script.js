c51c0488ac4669ec153b40045b3c26dc0ddb1577
federicodececco <federicodececco1@gmail.com>
2024-12-05 15:03:12 +0100

    es 04

--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -4,10 +4,18 @@ const userName = 'Mario';
 
 
 // Dichiara la funzione qui.
+/**
+ * Takes a string and returns a string that is the union of that input and the word "Ciao"
+ *@param{string} str - string to be concatenated
+ *@returns{string} - concatenated string
+**/
+const greetings = (str) => {
+    return `Ciao ${str}`
 
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(greetings(userName))
 
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
