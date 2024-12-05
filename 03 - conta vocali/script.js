598ce8b0fda61881ccf44d1788a1bc33a9ff0057
federicodececco <federicodececco1@gmail.com>
2024-12-05 14:56:33 +0100

    es 03

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -5,9 +5,26 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+/**
+ * Takes a string and counts how many vowels are in said string
+ *@param{string} str - string to be evaluated
+ *@returns{number} - number of vowels
+**/
+const numberOfVowels = (str) => {
+    let comparison = "aeiou"
+    counter = 0
+    for (let i = 0; i < str.length; i++) {
+        for (let k = 0; k < comparison.length; k++) {
+            if (comparison[k] === str[i]) {
+                counter++
+            }
+        }
+    }
+    return counter
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(numberOfVowels(word))
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
